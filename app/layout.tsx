b93115769f002a66d8c87312a53698f050d5adba
aismaanly <aisma.nurlaili24@gmail.com>
2025-01-21 13:26:14 +0700

    update backend contact

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
       <head />
       <ThemeProvider attribute='class' defaultTheme='light'>
         <body className={`${poppins.className} font-poppins bg-gray-100/50 dark:bg-grey-900 text-black dark:text-white overflow-x-hidden`}>
-          {/* <body className='bg-gray-100/50 dark:bg-grey-900 text-black dark:text-white overflow-x-hidden'> */}
           {children}
           <Analytics />
         </body>
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,7 +1,6 @@
-import axios from "axios";
 import { useState } from "react";
 import { BiLoaderAlt } from "react-icons/bi";
-import SectionWrapper from "./SectionWrapper"
+import SectionWrapper from "./SectionWrapper";
 import Image from "next/image";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
@@ -14,36 +13,6 @@ const Contact = () => {
         message: "",
     });
     const [loading, setLoading] = useState(false);
-    const [success, setSuccess] = useState(false);
-
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
-        if (!values.name.trim() || !values.email.trim() || !values.message.trim()) {
-            toast.warning("Empty Fields!")
-            return false;
-        }
-
-        setLoading(true);
-        axios.post("/api/mail", {
-            name: values.name,
-            email: values.email,
-            message: values.message,
-        }).then((res) => {
-            if (res.status === 200) {
-                setValues({ name: "", email: "", message: "" });
-                setLoading(false);
-                setSuccess(true);
-                toast.success(res.data.message)
-            } else {
-                setLoading(false);
-                toast.error(res.data.message)
-            }
-        }).catch((err) => {
-            setLoading(false);
-            toast.error(err.message)
-        });
-    };
 
     const handleChange = (e: | React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
         setValues((prevInput) => ({
@@ -63,18 +32,52 @@ const Contact = () => {
                     <h3 className="text-2xl">Get in touch</h3>
                     <p className="text-gray-400 mb-4 text-sm md:text-base">My inbox is always open! 💌 Whether you&apos;ve got a burning question or want to drop a friendly &quot;hello&quot;, I&apos;m all ears!👂 Let&apos;s chat! 🎉</p>
 
-                    <form onSubmit={handleSubmit} className="flex flex-col gap-4 rounded-xl">
-                        <input onChange={handleChange} required value={values.name} name="name" type="text" placeholder='Full Name *' className="outline-none bg-gray-100 dark:bg-grey-800 placeholder-gray-400 rounded-lg py-3 px-4" />
-                        <input onChange={handleChange} required value={values.email} name="email" type="email" placeholder='Email *' className="outline-none bg-gray-100 dark:bg-grey-800 placeholder-gray-400 rounded-lg py-3 px-4" />
-                        <textarea onChange={handleChange} required value={values.message} name="message" rows={4} placeholder='Message *' className="outline-none resize-none bg-gray-100 dark:bg-grey-800 placeholder-gray-400 rounded-lg py-3 px-4" />
-                        <button disabled={loading} className="px-4 py-2 bg-violet-600 hover:bg-violet-700 transition-colors text-white rounded-lg disabled:cursor-not-allowed self-end">
+                    <form 
+                        action="https://formsubmit.co/aisma.nurlaili24@gmail.com"
+                        method="POST" 
+                        className="flex flex-col gap-4 rounded-xl"
+                    >
+                        <input 
+                            onChange={handleChange} 
+                            required 
+                            value={values.name} 
+                            name="name" 
+                            type="text" 
+                            placeholder='Full Name *' 
+                            className="outline-none bg-gray-100 dark:bg-grey-800 placeholder-gray-400 rounded-lg py-3 px-4" 
+                        />
+                        <input 
+                            onChange={handleChange} 
+                            required 
+                            value={values.email} 
+                            name="email" 
+                            type="email" 
+                            placeholder='Email *' 
+                            className="outline-none bg-gray-100 dark:bg-grey-800 placeholder-gray-400 rounded-lg py-3 px-4" 
+                        />
+                        <textarea 
+                            onChange={handleChange} 
+                            required 
+                            value={values.message} 
+                            name="message" 
+                            rows={4} 
+                            placeholder='Message *' 
+                            className="outline-none resize-none bg-gray-100 dark:bg-grey-800 placeholder-gray-400 rounded-lg py-3 px-4" 
+                        />
+                        <input type="hidden" name="_captcha" value="false" />
+                        <input type="hidden" name="_template" value="table" />
+                        <button 
+                            type="submit" 
+                            disabled={loading} 
+                            className="px-4 py-2 bg-violet-600 hover:bg-violet-700 transition-colors text-white rounded-lg disabled:cursor-not-allowed self-end"
+                        >
                             {loading ? <span className="flex items-center gap-2">Say Hello <BiLoaderAlt className="animate-spin" /></span> : "Say Hello 👋"}
                         </button>
                     </form>
                 </div>
             </div>
-        </SectionWrapper >
+        </SectionWrapper>
     )
 }
 
-export default Contact
+export default Contact;

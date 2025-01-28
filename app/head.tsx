4cd477d5e3151ab06e4ec92810c793980c8a5533
aismaanly <aisma.nurlaili24@gmail.com>
2025-01-29 00:40:25 +0700

    check title

--- app/head.tsx
+++ app/head.tsx
@@ -2,20 +2,6 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="As an Information Systems undergraduate, I bring a passion for uncovering insights from data to fuel business growth." />
-      <meta name="keywords" content="aisma nurlaili, portofolio, aisma, data analyst, portfolio design, portfolio website, personal portfolio" />
-      <meta property="og:site_name" content="Aisma's Portfolio" />
-      <meta property="og:title" content="Portofolio | Aisma Nurlaili - Data Analyst" />
-      <meta property="og:description"
-        content="As an Information Systems undergraduate, I bring a passion for uncovering insights from data to fuel business growth." />
-      <meta property="og:url" content="https://portofolio-aisma-v1.vercel.app/" />
-      <link rel="canonical" href="https://portofolio-aisma-v1.vercel.app/" />
-      <meta name="author" content="Aisma Nurlaili" />
-      <meta name="language" content="English" />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="https://portofolio-aisma-v1.vercel.app/" />
-      <meta name="application-name" content="Portofolio | Aisma Nurlaili - Data Analyst" />
-      <meta name="apple-mobile-web-app-title" content="Portofolio | Aisma Nurlaili - Data Analyst" />
       <link rel="icon" href="/favicon.ico" />
       <title>Portofolio | Aisma Nurlaili - Data Analyst</title>
     </>
